54d3239e5230cd9d5c834327da520796d20d4581
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-12 09:06:00 -0700

    Created a new class and linked it to its viewController

---
+++ TrainingLog/Controller/RegisterPageViewController.swift
@@ -0,0 +1,28 @@
+//
+//  RegisterPageViewController.swift
+//  TrainingLog
+//
+//  Created by Reich, Brandon on 2/12/18.
+//  Copyright © 2018 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class RegisterPageViewController: UIViewController
+{
+    
+    
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning()
+    {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
