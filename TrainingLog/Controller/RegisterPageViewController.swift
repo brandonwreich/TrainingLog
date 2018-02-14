cad9bde5896cbca0a01430ca0f3d1e554594d896
Brandon Reich <31513789+brandonwreich@users.noreply.github.com>
2018-02-14 07:30:31 -0700

    Added methods to check for a valid password. Also added some documentation

--- TrainingLog/Controller/RegisterPageViewController.swift
+++ TrainingLog/Controller/RegisterPageViewController.swift
@@ -10,13 +10,22 @@ import UIKit
 
 class RegisterPageViewController: UIViewController
 {
+    // Storyboard outlets
     @IBOutlet weak var userEmailTextField: UITextField!
     @IBOutlet weak var userPasswordTextField: UITextField! 
     @IBOutlet weak var userConfirmPasswordTextField: UITextField!
     @IBOutlet weak var RegisterButton: UIButton!
     
-    var illegalCharacters: [String] = ["<", ">", "(", ")", "[", "]", ";", ","]
-    
+    /**
+     When the register button is clicked it first checks to see if all of the fields are filled. If
+     some are empty you get an error message. Then it checks to see if you have a valid email, checking
+     for a '@', and making sure you don't have any illegal characters such as '<' or '[' and also that
+     you don't start with any illegal characters. If any of the creitera is NOT met then it will display an
+     error message. It then checks to see if there is a valid password. A valid password contains 6-20
+     characters and a number. Next it checks to see if the userPassword field and the userConfirmPassword field
+     match, if not you get an error message. If all of those tests are passed the email and password are stored
+     using UserDefaults and you get a confirmation message showing you have successfully registered.
+ */
     @IBAction func registerButtonClicked(_ sender: Any)
     {
         // Initalize data members
@@ -35,14 +44,19 @@ class RegisterPageViewController: UIViewController
             return;
         }
         
+        // If the email contains a '@'
         if (!userEmail.contains("@"))
         {
+            // Display alert message 'Valid email required'
             displayMyAlertMessage(userMessage: "Vaild email required")
+            
+            // Set the password fields to empty
             userPasswordTextField.text = ""
             userConfirmPasswordTextField.text = ""
             
             return;
         }
+        // If the email contains any illegal characters
         else if (userEmail.contains("<") ||
             userEmail.contains(">") ||
             userEmail.contains("(") ||
@@ -53,12 +67,16 @@ class RegisterPageViewController: UIViewController
             userEmail.contains(",") ||
             userEmail.contains(" "))
         {
+            // Display alert message 'Valid email required'
             displayMyAlertMessage(userMessage: "Valid email required")
+            
+            // Set the password fields to empty
             userPasswordTextField.text = ""
             userConfirmPasswordTextField.text = ""
             
             return;
         }
+        // If the email starts with a number, hyphen, period, or underscore
         else if (userEmail.hasPrefix("_") ||
             userEmail.hasPrefix(".") ||
             userEmail.hasPrefix("-") ||
@@ -73,11 +91,45 @@ class RegisterPageViewController: UIViewController
             userEmail.hasPrefix("9") ||
             userEmail.hasPrefix("0"))
         {
+            // Display alert message 'Valid email required'
             displayMyAlertMessage(userMessage:"Valid email requird")
+            
+            // Set the password fields to empty
             userPasswordTextField.text = ""
             userConfirmPasswordTextField.text = ""
             
             return;
+        }
+        
+        // If the password is too short or too long
+        if (userPassword.count < 5 || userPassword.count > 20)
+        {
+            // Display alter message 'Password is too short'
+            displayMyAlertMessage(userMessage: "Passwords must be 6-20 characters long")
+            
+            // Set the password fields to empty
+            userPasswordTextField.text = ""
+            userConfirmPasswordTextField.text = ""
+            
+        }
+        // If the password does NOT contain a number
+        else if(!userPassword.contains("0") ||
+            !userPassword.contains("1") ||
+            !userPassword.contains("2") ||
+            !userPassword.contains("3") ||
+            !userPassword.contains("4") ||
+            !userPassword.contains("5") ||
+            !userPassword.contains("6") ||
+            !userPassword.contains("7") ||
+            !userPassword.contains("8") ||
+            !userPassword.contains("9"))
+        {
+            // Display alert message 'Password must contain a number'
+            displayMyAlertMessage(userMessage: "Passowords must contain at least one number")
+            
+            // Set the password fields to empty
+            userPasswordTextField.text = ""
+            userConfirmPasswordTextField.text = ""
         }
         
         // If the userPassword does not match the userConfirmPassword
@@ -85,6 +137,8 @@ class RegisterPageViewController: UIViewController
         {
             // Display the alert message
             displayMyAlertMessage(userMessage: "Passwords do not match")
+            
+            // Set the password fields to empty
             userPasswordTextField.text = ""
             userConfirmPasswordTextField.text = ""
             
